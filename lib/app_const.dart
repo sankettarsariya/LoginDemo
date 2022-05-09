6188c9a5b3ee45ef0db07e79a3d69ec1f983baf6
sankettarsariya <61108031+sankettarsariya@users.noreply.github.com>
2022-05-09 09:41:06 +0530

    Update app_const.dart

--- lib/app_const.dart
+++ lib/app_const.dart
@@ -40,5 +40,42 @@ class AppConst {
     }
   }
 
+  Future getPrefData({var key}) async {
+  SharedPreferences prefs = await SharedPreferences.getInstance();
+  var data = prefs.getString(key);
+  return data;
+}
+
+Future setPrefIntData({var key, var value}) async {
+  SharedPreferences prefs = await SharedPreferences.getInstance();
+  prefs.setInt(key, value);
+}
+
+class InitialData {
+  var deviceToken;
+  var deviceID;
+  var deviceType;
+  Future<void> getDeviceTypeId() async {
+    var deviceInfo = DeviceInfoPlugin();
+
+    if (Platform.isAndroid) {
+      deviceType = 0;
+      var androidDeviceInfo = await deviceInfo.androidInfo;
+      deviceID = androidDeviceInfo.androidId;
+      // print('Device Type: ${deviceType.toString()}, ' + 'Device ID: $deviceID');
+    } else {
+      deviceType = 1;
+      var iosDeviceInfo = await deviceInfo.iosInfo;
+      deviceID = iosDeviceInfo.identifierForVendor;
+      // print('Device Type: ${deviceType.toString()}, ' + 'Device ID: $deviceID');
+    }
+  }
+
+  getDeviceToken() {
+    var random = Random();
+    var values = List<int>.generate(200, (i) => random.nextInt(255));
+    deviceToken = base64UrlEncode(values);
+    // print('Device Token: ' + deviceToken);
+  }
   
 }
