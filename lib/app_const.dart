eb153f08c98fe06851a2aeeaf68ed0c23cdb302c
sankettarsariya <61108031+sankettarsariya@users.noreply.github.com>
2022-05-09 09:40:21 +0530

    Update app_const.dart

--- lib/app_const.dart
+++ lib/app_const.dart
@@ -6,4 +6,39 @@ class AppConst {
   static const String patient = "PATIENT";
   static const String odpMessage = "ODP";
   static const String textMessage = "TEXT";
+  
+    void loginUser() async {
+    try {
+      response = await dio.post(
+        'apiurl',
+        data: {
+          'email': emailController.text,
+          'password': passController.text,
+          'device_id': deviceID,
+          'device_token': deviceToken,
+          'device_type': deviceType,
+        },
+      );
+
+      if (response!.statusCode == 200) {
+        setState(() {
+          jsonData = jsonDecode(response.toString());
+        });
+        if (jsonData['status'] == 1) {
+          setUserData();
+          // Navigator.of(context).pushNamedAndRemoveUntil(
+          //     '/home', (Route<dynamic> route) => false);
+        }
+        if (jsonData['status'] == 0) {
+          // Toasty.showtoast(jsonData['message']);
+        }
+      } else {
+        return null;
+      }
+    } catch (e) {
+      print(e.toString());
+    }
+  }
+
+  
 }
